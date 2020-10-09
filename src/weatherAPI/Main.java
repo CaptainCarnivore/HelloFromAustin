812a0d52ccf9a73c7a21ac8fd7267485c9757e72
DallinPoole66 <72474685+DallinPoole66@users.noreply.github.com>
2020-10-09 16:24:14 -0600

    Updated From IntlliJ

--- src/weatherAPI/Main.java
+++ src/weatherAPI/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.print("Hello, my name is Austin Jensen.\n");
-	System.out.println("Hello Austin, my name is Dallin.");
+	System.out.println("Hello Austin, my name is Dallin!");
     }
 }
